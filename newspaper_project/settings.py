def2f97d52a44dea935512fae42e7a056001eceb
angelacpd <angelacrepaldi@gmail.com>
2021-07-24 17:53:36 -0300

    Create signup, login, and home pages.

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'newspaper_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
