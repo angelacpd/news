9dfc14f511de081dc446302dffb5920c77dbc95c
angelacpd <angelacrepaldi@gmail.com>
2021-07-25 23:10:41 -0300

    Add email provider SendGrid to send actual reset password emails to users.

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -137,4 +137,11 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
+DEFAULT_FROM_EMAIL = str(os.getenv('DEFAULT_FROM_EMAIL'))
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
