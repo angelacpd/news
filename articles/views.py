c1f72718aef03ff2cafc3e9e38fcde81e729a43c
angelacpd <angelacrepaldi@gmail.com>
2021-08-01 01:01:42 -0300

    Add ArticleDetailView, ArticleUpdateView, ArticleDeleteView andrespective templates.

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import DeleteView, UpdateView
+from django.urls import reverse_lazy
 
 from . import models
 
@@ -6,3 +8,20 @@ from . import models
 class ArticleListView(ListView):
     model = models.Article
     template_name = 'article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    model = models.Article
+    template_name = 'article_detail.html'
+
+
+class ArticleUpdateView(UpdateView):
+    model = models.Article
+    fields = ['title', 'body']
+    template_name = 'article_update.html'
+
+
+class ArticleDeleteView(DeleteView):
+    model = models.Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
