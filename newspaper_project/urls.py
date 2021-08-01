e8dfc9b9140c18fe6011885e091c814230817da1
angelacpd <angelacrepaldi@gmail.com>
2021-07-31 23:33:28 -0300

    Add Article List view, url, and template.

--- newspaper_project/urls.py
+++ newspaper_project/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
 ]
